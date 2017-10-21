a5e36c634240baa7586b08b1f0b3e59394a9bdbe
umichdoe <johndoe@umich.edu>
2017-10-20 18:57:15 -0700

    set up node/express server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//connect to Mongoose
+mongoose.connect('mongodb://localhost/fakeBookstore');
+var db = mongoose.connection
+
+app.get('/', function(req, res) {
+  res.send('please use /api/books or /api/genres');
+})
+
+
+var port = process.env.PORT || 3456;
+
+app.listen(port, function() {
+  console.log(`Running on port ${port}`);
+})
